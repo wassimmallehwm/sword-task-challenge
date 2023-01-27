9c3ce6665f18c5c17a92dbcbbddad098857af2c0
wassimmallehwm <wassimmalleh.wm@gmail.com>
2023-01-27 20:47:45 +0100

    separate app logic from server

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require("socket.io");
+require('dotenv').config();
+
+const PORT = process.env.PORT || 4000;
+
+const dbConnect = require('./database');
+const {expressMiddelwares} = require('./middleware');
+const ioConfig = require('./socket');
+
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+app.all("*",function(req, res, next){
+    req.io = io;
+    next();
+});
+
+expressMiddelwares(app, __dirname);
+dbConnect();
+
+ioConfig(io)
+
+module.exports = {
+    server,
+    app
+}
--- server/index.js
+++ server/index.js
@@ -1,34 +1,6 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const { Server } = require("socket.io");
-require('dotenv').config();
+const { server } = require("./app");
+const { PORT } = require("./config");
 
-const PORT = process.env.PORT || 4000;
-
-const dbConnect = require('./database');
-const {expressMiddelwares} = require('./middleware');
-const ioConfig = require('./socket');
-
-
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-    }
-});
-
-app.all("*",function(req, res, next){
-    req.io = io;
-    next();
-});
-
-expressMiddelwares(app, __dirname);
-dbConnect();
-
-ioConfig(io)
 server.listen(PORT, () => {
     console.log('Listening on port ' + PORT);
 });
-
-module.exports = app
